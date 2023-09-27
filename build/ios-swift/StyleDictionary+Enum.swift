bcb008b5792b402eafcc2b114738f0f8335f9b2f
Ridwanaam <ridwanaampkp@gmail.com>
2023-09-27 14:04:47 +0700

    updtae coolor

---
+++ build/ios-swift/StyleDictionary+Enum.swift
@@ -0,0 +1,97 @@
+
+//
+// StyleDictionary+Enum.swift
+//
+
+// Do not edit directly
+// Generated on Wed, 27 Sep 2023 06:37:16 GMT
+
+
+import UIKit
+
+public enum StyleDictionaryEnum {
+    public static let colorBaseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha: 1)
+    public static let colorBaseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    public static let colorBaseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
+    public static let colorBaseGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    public static let colorBaseRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let colorFontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let colorFontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    public static let colorFontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    public static let globalAamGlobalColorDangerDangerBase = #e93426
+    public static let globalAamGlobalColorDangerDangerDark = #c81b1d
+    public static let globalAamGlobalColorDangerDangerDarker = #6f0722
+    public static let globalAamGlobalColorDangerDangerLight = #f1715a
+    public static let globalAamGlobalColorDangerDangerLighter = #f8997b
+    public static let globalAamGlobalColorInfoInfoBase = #3aa0ff
+    public static let globalAamGlobalColorInfoInfoDark = #2a7cdb
+    public static let globalAamGlobalColorInfoInfoDarker = #0b2d7a
+    public static let globalAamGlobalColorInfoInfoLight = #6bbfff
+    public static let globalAamGlobalColorInfoInfoLighter = #88d2ff
+    public static let globalAamGlobalColorNeutralNeutralBase = #7a7a7a
+    public static let globalAamGlobalColorNeutralNeutralDark = #404040
+    public static let globalAamGlobalColorNeutralNeutralDarker = #070707
+    public static let globalAamGlobalColorNeutralNeutralLight = #f2f2f2
+    public static let globalAamGlobalColorNeutralNeutralLighter = #f5f8ff
+    public static let globalAamGlobalColorNeutralNeutralLightess = #f4f4fc
+    public static let globalAamGlobalColorNeutralNeutralWhite = #ffffff
+    public static let globalAamGlobalColorPrimaryAzureBase = #b20aff
+    public static let globalAamGlobalColorPrimaryAzureDark = #075edb
+    public static let globalAamGlobalColorPrimaryAzureDarker = #063c98
+    public static let globalAamGlobalColorPrimaryAzureLight = #47a3ff
+    public static let globalAamGlobalColorPrimaryAzureLighter = #9cd7ff
+    public static let globalAamGlobalColorSecondaryGoldenBase = #ffd600
+    public static let globalAamGlobalColorSecondaryGoldenDark = #dbb400
+    public static let globalAamGlobalColorSecondaryGoldenDarker = #7a5e00
+    public static let globalAamGlobalColorSecondaryGoldenLight = #ffe766
+    public static let globalAamGlobalColorSecondaryGoldenLighter = #ffef99
+    public static let globalAamGlobalColorSecondaryGreenPigmentBase = #00a650
+    public static let globalAamGlobalColorSecondaryGreenPigmentDark = #00803d
+    public static let globalAamGlobalColorSecondaryGreenPigmentDarker = #004f46
+    public static let globalAamGlobalColorSecondaryGreenPigmentLight = #00e66f
+    public static let globalAamGlobalColorSecondaryGreenPigmentLighter = #99ffca
+    public static let globalAamGlobalColorSecondaryRaspberryBase = #ff0060
+    public static let globalAamGlobalColorSecondaryRaspberryDark = #b70069
+    public static let globalAamGlobalColorSecondaryRaspberryDarker = #7a005e
+    public static let globalAamGlobalColorSecondaryRaspberryLight = #ff80ae
+    public static let globalAamGlobalColorSecondaryRaspberryLighter = #ffccdf
+    public static let globalAamGlobalColorSecondarySteelBase = #388aa3
+    public static let globalAamGlobalColorSecondarySteelDark = #286d8c
+    public static let globalAamGlobalColorSecondarySteelDarker = #143b53
+    public static let globalAamGlobalColorSecondarySteelLight = #65b7c7
+    public static let globalAamGlobalColorSecondarySteelLighter = #89dbe3
+    public static let globalAamGlobalColorSecondaryUltramarineBase = #4a64ff
+    public static let globalAamGlobalColorSecondaryUltramarineDark = #364bdb
+    public static let globalAamGlobalColorSecondaryUltramarineDarker = #172393
+    public static let globalAamGlobalColorSecondaryUltramarineLight = #778cff
+    public static let globalAamGlobalColorSecondaryUltramarineLighter = #dae2ff
+    public static let globalAamGlobalColorSuccessSuccessBase = #00b077
+    public static let globalAamGlobalColorSuccessSuccessDark = #009775
+    public static let globalAamGlobalColorSuccessSuccessDarker = #004e54
+    public static let globalAamGlobalColorSuccessSuccessLight = #33cf8d
+    public static let globalAamGlobalColorSuccessSuccessLighter = #5ce79f
+    public static let globalAamGlobalColorWarningWarningBase = #fcc047
+    public static let globalAamGlobalColorWarningWarningDark = #b57b23
+    public static let globalAamGlobalColorWarningWarningDarker = #74430f
+    public static let globalAamGlobalColorWarningWarningLight = #ffce6b
+    public static let globalAamGlobalColorWarningWarningLighter = #fee190
+    public static let globalAamThemaColorBackgroundBackgroundPrimary = #eceff3
+    public static let globalAamThemaColorBackgroundBackgroundSecondary = #f5f8ff
+    public static let globalAamThemaColorBackgroundBackgroundWhite = #ffffff
+    public static let globalAamThemaColorBorderBorder1 = #f2f2f2
+    public static let globalAamThemaColorBorderBorder2 = #f5f8ff
+    public static let globalAamThemaColorBorderBorderActive = #0a7aff
+    public static let globalAamThemaColorBorderBorderError = #e93426
+    public static let globalAamThemaColorContentContentPrimary = #070707
+    public static let globalAamThemaColorContentContentSecondary = #404040
+    public static let globalAamThemaColorContentContentTertiary = #7a7a7a
+    public static let globalAamThemaColorContentContentWhite = #ffffff
+    public static let globalAamThemaColorDeviderDevider1 = #f2f2f2
+    public static let globalAamThemaColorDeviderDevider2 = #f5f8ff
+    public static let globalLinear2PutarAyat = linear-gradient(90deg, #ffc80b 0%, #e89318 100%)
+    public static let globalLinearPutarAyat = linear-gradient(241deg, #283663 0%, #6a769f 100%)
+    public static let sizeFontBase = CGFloat(16.00) /* the base size of the font */
+    public static let sizeFontLarge = CGFloat(32.00) /* the large size of the font */
+    public static let sizeFontMedium = CGFloat(16.00) /* the medium size of the font */
+    public static let sizeFontSmall = CGFloat(12.00) /* the small size of the font */
+}
---
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -0,0 +1,97 @@
+
+//
+// StyleDictionary+Struct.swift
+//
+
+// Do not edit directly
+// Generated on Wed, 27 Sep 2023 06:37:16 GMT
+
+
+import UIKit
+
+internal struct StyleDictionaryStruct {
+    internal static let colorBaseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha: 1)
+    internal static let colorBaseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    internal static let colorBaseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
+    internal static let colorBaseGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    internal static let colorBaseRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    internal static let colorFontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    internal static let colorFontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    internal static let colorFontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    internal static let globalAamGlobalColorDangerDangerBase = #e93426
+    internal static let globalAamGlobalColorDangerDangerDark = #c81b1d
+    internal static let globalAamGlobalColorDangerDangerDarker = #6f0722
+    internal static let globalAamGlobalColorDangerDangerLight = #f1715a
+    internal static let globalAamGlobalColorDangerDangerLighter = #f8997b
+    internal static let globalAamGlobalColorInfoInfoBase = #3aa0ff
+    internal static let globalAamGlobalColorInfoInfoDark = #2a7cdb
+    internal static let globalAamGlobalColorInfoInfoDarker = #0b2d7a
+    internal static let globalAamGlobalColorInfoInfoLight = #6bbfff
+    internal static let globalAamGlobalColorInfoInfoLighter = #88d2ff
+    internal static let globalAamGlobalColorNeutralNeutralBase = #7a7a7a
+    internal static let globalAamGlobalColorNeutralNeutralDark = #404040
+    internal static let globalAamGlobalColorNeutralNeutralDarker = #070707
+    internal static let globalAamGlobalColorNeutralNeutralLight = #f2f2f2
+    internal static let globalAamGlobalColorNeutralNeutralLighter = #f5f8ff
+    internal static let globalAamGlobalColorNeutralNeutralLightess = #f4f4fc
+    internal static let globalAamGlobalColorNeutralNeutralWhite = #ffffff
+    internal static let globalAamGlobalColorPrimaryAzureBase = #b20aff
+    internal static let globalAamGlobalColorPrimaryAzureDark = #075edb
+    internal static let globalAamGlobalColorPrimaryAzureDarker = #063c98
+    internal static let globalAamGlobalColorPrimaryAzureLight = #47a3ff
+    internal static let globalAamGlobalColorPrimaryAzureLighter = #9cd7ff
+    internal static let globalAamGlobalColorSecondaryGoldenBase = #ffd600
+    internal static let globalAamGlobalColorSecondaryGoldenDark = #dbb400
+    internal static let globalAamGlobalColorSecondaryGoldenDarker = #7a5e00
+    internal static let globalAamGlobalColorSecondaryGoldenLight = #ffe766
+    internal static let globalAamGlobalColorSecondaryGoldenLighter = #ffef99
+    internal static let globalAamGlobalColorSecondaryGreenPigmentBase = #00a650
+    internal static let globalAamGlobalColorSecondaryGreenPigmentDark = #00803d
+    internal static let globalAamGlobalColorSecondaryGreenPigmentDarker = #004f46
+    internal static let globalAamGlobalColorSecondaryGreenPigmentLight = #00e66f
+    internal static let globalAamGlobalColorSecondaryGreenPigmentLighter = #99ffca
+    internal static let globalAamGlobalColorSecondaryRaspberryBase = #ff0060
+    internal static let globalAamGlobalColorSecondaryRaspberryDark = #b70069
+    internal static let globalAamGlobalColorSecondaryRaspberryDarker = #7a005e
+    internal static let globalAamGlobalColorSecondaryRaspberryLight = #ff80ae
+    internal static let globalAamGlobalColorSecondaryRaspberryLighter = #ffccdf
+    internal static let globalAamGlobalColorSecondarySteelBase = #388aa3
+    internal static let globalAamGlobalColorSecondarySteelDark = #286d8c
+    internal static let globalAamGlobalColorSecondarySteelDarker = #143b53
+    internal static let globalAamGlobalColorSecondarySteelLight = #65b7c7
+    internal static let globalAamGlobalColorSecondarySteelLighter = #89dbe3
+    internal static let globalAamGlobalColorSecondaryUltramarineBase = #4a64ff
+    internal static let globalAamGlobalColorSecondaryUltramarineDark = #364bdb
+    internal static let globalAamGlobalColorSecondaryUltramarineDarker = #172393
+    internal static let globalAamGlobalColorSecondaryUltramarineLight = #778cff
+    internal static let globalAamGlobalColorSecondaryUltramarineLighter = #dae2ff
+    internal static let globalAamGlobalColorSuccessSuccessBase = #00b077
+    internal static let globalAamGlobalColorSuccessSuccessDark = #009775
+    internal static let globalAamGlobalColorSuccessSuccessDarker = #004e54
+    internal static let globalAamGlobalColorSuccessSuccessLight = #33cf8d
+    internal static let globalAamGlobalColorSuccessSuccessLighter = #5ce79f
+    internal static let globalAamGlobalColorWarningWarningBase = #fcc047
+    internal static let globalAamGlobalColorWarningWarningDark = #b57b23
+    internal static let globalAamGlobalColorWarningWarningDarker = #74430f
+    internal static let globalAamGlobalColorWarningWarningLight = #ffce6b
+    internal static let globalAamGlobalColorWarningWarningLighter = #fee190
+    internal static let globalAamThemaColorBackgroundBackgroundPrimary = #eceff3
+    internal static let globalAamThemaColorBackgroundBackgroundSecondary = #f5f8ff
+    internal static let globalAamThemaColorBackgroundBackgroundWhite = #ffffff
+    internal static let globalAamThemaColorBorderBorder1 = #f2f2f2
+    internal static let globalAamThemaColorBorderBorder2 = #f5f8ff
+    internal static let globalAamThemaColorBorderBorderActive = #0a7aff
+    internal static let globalAamThemaColorBorderBorderError = #e93426
+    internal static let globalAamThemaColorContentContentPrimary = #070707
+    internal static let globalAamThemaColorContentContentSecondary = #404040
+    internal static let globalAamThemaColorContentContentTertiary = #7a7a7a
+    internal static let globalAamThemaColorContentContentWhite = #ffffff
+    internal static let globalAamThemaColorDeviderDevider1 = #f2f2f2
+    internal static let globalAamThemaColorDeviderDevider2 = #f5f8ff
+    internal static let globalLinear2PutarAyat = linear-gradient(90deg, #ffc80b 0%, #e89318 100%)
+    internal static let globalLinearPutarAyat = linear-gradient(241deg, #283663 0%, #6a769f 100%)
+    internal static let sizeFontBase = CGFloat(16.00) /* the base size of the font */
+    internal static let sizeFontLarge = CGFloat(32.00) /* the large size of the font */
+    internal static let sizeFontMedium = CGFloat(16.00) /* the medium size of the font */
+    internal static let sizeFontSmall = CGFloat(12.00) /* the small size of the font */
+}
